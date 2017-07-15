ae6c0168c8642f07ee04d90e00668388b89acb66
Eric Pan <Eric Pan>
2017-07-15 13:09:21 -0400

    2nd update

--- tapp.js
+++ tapp.js
@@ -21,8 +21,10 @@ alert("Hello");
 
 var name = $("#Train-Name").val().trim();
 var des = $("#Destination").val().trim();
-var time = $("#Military-Time").val().trim();
+var time = moment($("#Military-Time").val().trim(), "HH:mm").format("X");
 var frequency = $("#Frequency").val().trim();
+
+
 
 database.ref().push({
       Tname: name,
@@ -41,19 +43,24 @@ alert("train scheduled");
 
 database.ref().on("child_added", function(childSnap) {
 
-  // Store everything into a variable.
   var name = childSnap.val().Tname;
   var des = childSnap.val().destination;
   var time = childSnap.val().time;
   var frequency = childSnap.val().frequency;
+  var current = moment(time, "hh:mm").subtract(1, "years"); 
+  var diffTime = moment().diff(moment(current), "minutes");
+  var Remainder = diffTime % frequency;
+  var minutes = frequency - Remainder;
+  var nextArrival = moment(diffTime).format("HH:mm"); 
+
 
   console.log(name);
   console.log(des);
   console.log(time);
   console.log(frequency);
+  console.log(moment());
 
-  var timeFormat = moment.unix(time).format("MM/DD/YY");
 
   $("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + des + "</td><td>" +
-  frequency + "</td><td>" + "next arrival" + "</td><td>" + "minutes away" + "</td></tr>");
+  frequency + "</td><td>" + nextArrival + "</td><td>" + minutes + "</td></tr>");
 });
